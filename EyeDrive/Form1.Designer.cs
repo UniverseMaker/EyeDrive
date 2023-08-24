07948e80546ab3195705df9149ebce38b62934f8
Daeseung Park <eosonox@gmail.com>
2023-08-24 15:08:46 +0900

    Version 1

---
+++ EyeDrive/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace EyeDrive
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.btnConnect = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnDisconnect = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPW = new System.Windows.Forms.TextBox();
+            this.chkDebug = new System.Windows.Forms.CheckBox();
+            this.chkWindowsRW = new System.Windows.Forms.CheckBox();
+            this.txtCache = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.chkCMDHide = new System.Windows.Forms.CheckBox();
+            this.txtMount = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblStatus2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Server";
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(15, 60);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(366, 28);
+            this.txtServer.TabIndex = 1;
+            this.txtServer.Text = "https://.synology.com:5006";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnConnect,
+            this.btnDisconnect});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(393, 33);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(95, 29);
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(118, 29);
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus,
+            this.lblStatus2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 231);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(393, 32);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(15, 117);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(177, 28);
+            this.txtID.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "ID && Password";
+            // 
+            // txtPW
+            // 
+            this.txtPW.Location = new System.Drawing.Point(198, 117);
+            this.txtPW.Name = "txtPW";
+            this.txtPW.PasswordChar = '#';
+            this.txtPW.Size = new System.Drawing.Size(183, 28);
+            this.txtPW.TabIndex = 6;
+            // 
+            // chkDebug
+            // 
+            this.chkDebug.AutoSize = true;
+            this.chkDebug.Location = new System.Drawing.Point(15, 208);
+            this.chkDebug.Name = "chkDebug";
+            this.chkDebug.Size = new System.Drawing.Size(127, 22);
+            this.chkDebug.TabIndex = 7;
+            this.chkDebug.Text = "Debug(Log)";
+            this.chkDebug.UseVisualStyleBackColor = true;
+            // 
+            // chkWindowsRW
+            // 
+            this.chkWindowsRW.AutoSize = true;
+            this.chkWindowsRW.Checked = true;
+            this.chkWindowsRW.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkWindowsRW.Location = new System.Drawing.Point(148, 208);
+            this.chkWindowsRW.Name = "chkWindowsRW";
+            this.chkWindowsRW.Size = new System.Drawing.Size(94, 22);
+            this.chkWindowsRW.TabIndex = 8;
+            this.chkWindowsRW.Text = "Win RW";
+            this.chkWindowsRW.UseVisualStyleBackColor = true;
+            // 
+            // txtCache
+            // 
+            this.txtCache.Location = new System.Drawing.Point(15, 174);
+            this.txtCache.Name = "txtCache";
+            this.txtCache.Size = new System.Drawing.Size(213, 28);
+            this.txtCache.TabIndex = 10;
+            this.txtCache.Text = "Z:\\Cache";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 18);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Cache";
+            // 
+            // chkCMDHide
+            // 
+            this.chkCMDHide.AutoSize = true;
+            this.chkCMDHide.Checked = true;
+            this.chkCMDHide.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkCMDHide.Location = new System.Drawing.Point(248, 208);
+            this.chkCMDHide.Name = "chkCMDHide";
+            this.chkCMDHide.Size = new System.Drawing.Size(112, 22);
+            this.chkCMDHide.TabIndex = 11;
+            this.chkCMDHide.Text = "Hide CMD";
+            this.chkCMDHide.UseVisualStyleBackColor = true;
+            // 
+            // txtMount
+            // 
+            this.txtMount.Location = new System.Drawing.Point(234, 174);
+            this.txtMount.Name = "txtMount";
+            this.txtMount.Size = new System.Drawing.Size(147, 28);
+            this.txtMount.TabIndex = 13;
+            this.txtMount.Text = "Y:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(231, 153);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 18);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Mount";
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(179, 25);
+            this.lblStatus.Text = "Wait for Connection.";
+            // 
+            // lblStatus2
+            // 
+            this.lblStatus2.Name = "lblStatus2";
+            this.lblStatus2.Size = new System.Drawing.Size(166, 25);
+            this.lblStatus2.Text = "| P-Watcher Ready.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(393, 263);
+            this.Controls.Add(this.txtMount);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.chkCMDHide);
+            this.Controls.Add(this.txtCache);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.chkWindowsRW);
+            this.Controls.Add(this.chkDebug);
+            this.Controls.Add(this.txtPW);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.txtServer);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Eyedrive";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPW;
+        private System.Windows.Forms.CheckBox chkDebug;
+        private System.Windows.Forms.CheckBox chkWindowsRW;
+        private System.Windows.Forms.TextBox txtCache;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ToolStripMenuItem btnConnect;
+        private System.Windows.Forms.CheckBox chkCMDHide;
+        private System.Windows.Forms.TextBox txtMount;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ToolStripMenuItem btnDisconnect;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus2;
+    }
+}
+
---
+++ EyeDrive/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace EyeDrive
+{
+    public partial class Form1 : Form
+    {
+        System.Threading.Thread th = null;
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            System.Diagnostics.Process.GetCurrentProcess().Kill();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if(txtServer.Text == "" || txtID.Text == "" || txtPW.Text == "")
+            {
+                MessageBox.Show("서버, 아이디, 암호를 입력해주세요");
+                return;
+            }
+            if(txtMount.Text == "" || txtMount.Text.IndexOf("\\") != -1)
+            {
+                MessageBox.Show("마운트 정보가 잘못되었습니다");
+                return;
+            }
+            if(th != null)
+            {
+                MessageBox.Show("연결을 먼저 해제하세요");
+                return;
+            }
+
+            lblStatus.Text = "Connecting...";
+            Application.DoEvents();
+
+            string pw = cmdExecute("rclone.exe obscure " + txtPW.Text);
+            label2.Text = "ID && Password (" + pw + ")";
+
+            string opt = " :webdav:/ "+txtMount.Text+" --webdav-url="+txtServer.Text+ " --webdav-user="+txtID.Text+" --webdav-pass="+pw+" --vfs-cache-mode=full --webdav-vendor=other";
+            if (txtCache.Text != "")
+                opt += " --cache-dir=" + txtCache.Text;
+            if (chkDebug.Checked)
+                opt += " --log-file log.txt --log-level=DEBUG";
+            if (chkWindowsRW.Checked)
+                opt += " -o FileSecurity=\"D:P(A;;FA;;;WD)\"";
+
+            //th = new System.Threading.Thread(() => connectWebDav(opt, chkCMDHide.Checked));
+            connectWebDav(opt, chkCMDHide.Checked);
+            timer.Enabled = true;
+
+            lblStatus.Text = "Connected.";
+            Application.DoEvents();
+        }
+
+        public void enableSystem()
+        {
+            txtServer.Enabled = true;
+            txtID.Enabled = true;
+            txtPW.Enabled = true;
+            txtMount.Enabled = true;
+            txtCache.Enabled = true;
+            chkDebug.Enabled = true;
+            chkWindowsRW.Enabled = true;
+            chkCMDHide.Enabled = true;
+        }
+
+        public void disableSystem()
+        {
+            txtServer.Enabled = false;
+            txtID.Enabled = false;
+            txtPW.Enabled = false;
+            txtMount.Enabled = false;
+            txtCache.Enabled = false;
+            chkDebug.Enabled = false;
+            chkWindowsRW.Enabled = false;
+            chkCMDHide.Enabled = false;
+        }
+
+        public string cmdExecute(string data, bool hide = true)
+        {
+            ProcessStartInfo cmd = new ProcessStartInfo();
+            Process process = new Process();
+            cmd.FileName = @"cmd";
+            if (hide == true)
+            {
+                cmd.WindowStyle = ProcessWindowStyle.Hidden;             // cmd창이 숨겨지도록 하기
+                cmd.CreateNoWindow = true;                               // cmd창을 띄우지 안도록 하기
+            }
+            else
+            {
+                cmd.WindowStyle = ProcessWindowStyle.Normal;             // cmd창이 숨겨지도록 하기
+                cmd.CreateNoWindow = false;                               // cmd창을 띄우지 안도록 하기
+            }
+
+            cmd.UseShellExecute = false;
+            cmd.RedirectStandardOutput = true;
+            cmd.RedirectStandardInput = true;
+            cmd.RedirectStandardError = true;
+
+            process.EnableRaisingEvents = false;
+            process.StartInfo = cmd;
+            process.Start();
+            process.StandardInput.Write(data + Environment.NewLine);
+            process.StandardInput.Close();
+
+            string result = process.StandardOutput.ReadToEnd();
+            string prs = "";
+            string src = "";
+            foreach (string line in System.Text.RegularExpressions.Regex.Split(result, "\r\n"))
+            {
+                if (line.IndexOf(data) != -1)
+                    src = line.Replace(data, "");
+
+                if (src != "" && line.IndexOf(data) == -1 && line.IndexOf(src) == -1 && line != "")
+                    prs += line.Replace("\r", "").Replace("\n", "");
+
+                if (src != "" && line.IndexOf(data) == -1 && line.IndexOf(src) != -1 && line != "")
+                    break;
+            }
+
+            process.WaitForExit();
+            process.Close();
+
+            return prs;
+        }
+
+        ProcessStartInfo cmd_con = null;
+        Process process_con = null;
+        bool discon = false;
+        public void connectWebDav(string opt, bool hide)
+        {
+            cmd_con = new ProcessStartInfo();
+            process_con = new Process();
+
+            cmd_con.FileName = @"cmd";
+            if (hide == true)
+            {
+                cmd_con.WindowStyle = ProcessWindowStyle.Hidden;             // cmd창이 숨겨지도록 하기
+                cmd_con.CreateNoWindow = true;                               // cmd창을 띄우지 안도록 하기
+            }
+            else
+            {
+                cmd_con.WindowStyle = ProcessWindowStyle.Normal;             // cmd창이 숨겨지도록 하기
+                cmd_con.CreateNoWindow = false;                               // cmd창을 띄우지 안도록 하기
+            }
+
+            cmd_con.UseShellExecute = false;
+            cmd_con.RedirectStandardOutput = true;
+            cmd_con.RedirectStandardInput = true;
+            cmd_con.RedirectStandardError = true;
+
+            process_con.EnableRaisingEvents = false;
+            process_con.StartInfo = cmd_con;
+            process_con.Start();
+            process_con.StandardInput.Write("rclone.exe mount" + opt + Environment.NewLine);
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            Process[] pname = Process.GetProcessesByName("rclone");
+            if (pname.Length > 0)
+                lblStatus2.Text = "P-Detect " + pname.Length.ToString();
+            else
+                lblStatus2.Text = "P-Watcher Ready.";
+
+            Application.DoEvents();
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            lblStatus.Text = "Disconnecting...";
+            Application.DoEvents();
+
+            process_con.StandardInput.Close();
+
+            int kcc = 0;
+            Process[] pname = Process.GetProcessesByName("rclone");
+            while (pname.Length > 0)
+            {
+                kcc++;
+                process_con.StandardInput.Close();
+                pname = Process.GetProcessesByName("rclone");
+                System.Threading.Thread.Sleep(0);
+
+                if (kcc > 50)
+                {
+                    foreach (Process p in pname)
+                    {
+                        p.Kill();
+                        System.Threading.Thread.Sleep(0);
+                    }
+
+                    break;
+                }
+            }
+
+            cmd_con = null;
+            process_con = null;
+
+            lblStatus.Text = "Disconnected.";
+            Application.DoEvents();
+        }
+    }
+}
